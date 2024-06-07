cc07d778255ae109536541fe9da921c693234a9e
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-06-07 11:12:24 +0200

    Add DictionaryAPI

---
+++ app/api/DictionaryAPI/Dictionary.api.ts
@@ -0,0 +1,18 @@
+export const provinces = [
+  { value: '01', label: 'dolnośląskie' },
+  { value: '02', label: 'kujawsko-pomorskie' },
+  { value: '03', label: 'lubelskie' },
+  { value: '04', label: 'lubuskie' },
+  { value: '05', label: 'łódzkie' },
+  { value: '06', label: 'małopolskie' },
+  { value: '07', label: 'mazowieckie' },
+  { value: '08', label: 'opolskie' },
+  { value: '09', label: 'podkarpackie' },
+  { value: '10', label: 'podlaskie' },
+  { value: '11', label: 'pomorskie' },
+  { value: '12', label: 'śląskie' },
+  { value: '13', label: 'świętokrzyskie' },
+  { value: '14', label: 'warmińsko-mazurskie' },
+  { value: '15', label: 'wielkopolskie' },
+  { value: '16', label: 'zachodniopomorskie' }
+];
