4dd7371aa21febee525bb918a62c0a746cfa813c
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-06-11 22:27:42 +0200

    Change primary color and add select to form

--- app/components/SearchTerm/SearchTerm.tsx
+++ app/components/SearchTerm/SearchTerm.tsx
@@ -1,70 +1,64 @@
 'use client';
 
-import { Check, ChevronsUpDown } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 
 import { provinces } from '@/api/DictionaryAPI/Dictionary.api';
 import { QueueData } from '@/api/QueueAPI/Queue.model';
 import { SearchParams } from '@/api/QueueAPI/Queue.model';
-import { Combobox } from '@/components';
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  Popover,
-  PopoverContent,
-  PopoverTrigger
+  Button,
+  RadioGroup,
+  RadioGroupItem,
+  Checkbox,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Input
 } from '@/components/ui/';
-import { Button, Label, RadioGroup, RadioGroupItem, Checkbox } from '@/components/ui/';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { useGetQueue } from '@/hooks/useGetQueue';
-import { cn } from '@/lib/utils';
 
+type Suggestions = {
+  value: string;
+  label: string;
+}[];
 export const SearchTerm = () => {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('');
-  const [suggestions, setSuggestions] = useState<string[]>([]);
-  const [suggestionsLocalities, setSuggestionsLocalities] = useState<string[]>([]);
+  const [suggestions, setSuggestions] = useState<Suggestions>([]);
+  const [suggestionsLocalities, setSuggestionsLocalities] = useState<Suggestions>([]);
   const [searchParams, setSearchParams] = useState<SearchParams | null>(null);
   const { data: QueueList, isLoading, isError } = useGetQueue(searchParams);
+  const form = useForm<SearchParams>({
+    defaultValues: {
+      urgent: '1',
+      benefitForChildren: false
+    }
+  });
   const onHandleSearch = (data: SearchParams) => {
     setSearchParams(data);
   };
-
-  console.log(QueueList);
 
   useEffect(() => {
     setSearchParams(null);
   }, [isLoading]);
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors }
-  } = useForm<SearchParams>({
-    defaultValues: {
-      urgent: '1'
-    }
-  });
-
-  const query = watch('query');
-  const queryCity = watch('localities');
-  const province = watch('province');
-
-  console.log(province);
-
   useEffect(() => {
-    const subscription = watch((value) => {
-      if (value.query && value.query.length >= 3) {
+    const subscription = form.watch((value) => {
+      if (value.benefit && value.benefit.length >= 3) {
         // Fetch suggestions from the backend
         fetch(
-          `https://api.nfz.gov.pl/app-itl-api/benefits?page=1&limit=10&format=json&name=${value.query}&api-version=1.3`
+          `https://api.nfz.gov.pl/app-itl-api/benefits?page=1&limit=10&format=json&name=${value.benefit}&api-version=1.3`
         )
           .then((response) => response.json())
-          .then((data) => setSuggestions(data.data))
+          .then((data) => {
+            const mappedSuggestions = data.data.map((item: string) => ({
+              value: item,
+              label: item
+            }));
+            setSuggestions(mappedSuggestions);
+          })
           .catch((error) => console.error('Error fetching suggestions:', error));
       } else {
         setSuggestions([]);
@@ -72,18 +66,22 @@ export const SearchTerm = () => {
     });
 
     return () => subscription.unsubscribe();
-  }, [query]);
+  }, [form.watch('benefit')]);
 
   useEffect(() => {
-    const subscriptionCity = watch((value) => {
-      if (value.queryCity && value.queryCity.length >= 3) {
-        // Fetch suggestions from the backend
-
+    const subscriptionCity = form.watch((value) => {
+      if (value.localities && value.localities.length >= 3) {
         fetch(
-          `https://api.nfz.gov.pl/app-itl-api/localities?page=1&limit=10&format=json&name=${value.queryCity}&province=11&api-version=1.3`
+          `https://api.nfz.gov.pl/app-itl-api/localities?page=1&limit=10&format=json&name=${value.localities}&province=11&api-version=1.3`
         )
           .then((response) => response.json())
-          .then((data) => setSuggestionsLocalities(data.data))
+          .then((data) => {
+            const mappedSuggestions = data.data.map((item: string) => ({
+              value: item,
+              label: item
+            }));
+            setSuggestionsLocalities(mappedSuggestions);
+          })
           .catch((error) => console.error('Error fetching suggestions:', error));
       } else {
         setSuggestionsLocalities([]);
@@ -91,82 +89,141 @@ export const SearchTerm = () => {
     });
 
     return () => subscriptionCity.unsubscribe();
-  }, [queryCity]);
+  }, [form.watch('localities')]);
 
   const onSubmit: SubmitHandler<SearchParams> = (data) => {
+    console.log(data);
+
     onHandleSearch(data);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col gap-4">
-          <div className="flex gap-4">
-            <Label htmlFor="urgent1">przypadek:</Label>
-            <RadioGroup defaultValue="1">
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="1" id="urgent1" {...register('urgent')} />
-                <Label htmlFor="urgent1">stabilny</Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="2" id="urgent1" {...register('urgent')} />
-                <Label htmlFor="urgent2">pilny</Label>
-              </div>
-            </RadioGroup>
-          </div>
-          <div className="flex items-center space-x-2">
-            <Checkbox id="benefitForChildren" {...register('benefitForChildren')} />
-            <label
-              htmlFor="benefitForChildren"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-              świadczenia udzielane dzieciom
-            </label>
-          </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="urgent"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Przypadek</FormLabel>
+                <FormControl>
+                  <RadioGroup onValueChange={field.onChange} defaultValue="1" className="flex flex-col space-y-1">
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="1" />
+                      </FormControl>
+                      <FormLabel className="font-normal">stabilny</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="2" />
+                      </FormControl>
+                      <FormLabel className="font-normal">pilny</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="benefitForChildren"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow">
+                <FormControl>
+                  <Checkbox checked={field.value} defaultValue={0} onCheckedChange={field.onChange} />
+                </FormControl>
+                <div className="space-y-1 leading-none">
+                  <FormLabel>świadczenia udzielane dzieciom</FormLabel>
+                  <FormDescription>Zaznacz jeśli szukasz świadczenia dla dziecka</FormDescription>
+                </div>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="province"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Województwo</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Wybierz województwo" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {provinces.map((province) => (
+                      <SelectItem value={province.value} key={province.value}>
+                        {province.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormDescription>Wybierz województwo.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div>
-            <Combobox register={register} options={provinces} placeholder="Wybirze województwo" />
-            {errors.province && <span>This field is required</span>}
+            <FormField
+              control={form.control}
+              name="benefit"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Usługa</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Wybierz usługę" {...field} />
+                  </FormControl>
+                  <FormDescription>Wybierz usługę.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div>
+              <ul>
+                {suggestions.length > 0 &&
+                  suggestions.map((suggestion) => (
+                    <li key={suggestion.value} onClick={() => form.setValue('benefit', suggestion.value)}>
+                      {suggestion.value}
+                    </li>
+                  ))}
+              </ul>
+            </div>
           </div>
+
           <div>
-            <label>Query usługa:</label>
-            <input type="text" {...register('query')} />
+            <FormField
+              control={form.control}
+              name="localities"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Miasto</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Wybierz miasto" {...field} />
+                  </FormControl>
+                  <FormDescription>Wybierz miasto.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div>
+              <ul>
+                {suggestionsLocalities.length > 0 &&
+                  suggestionsLocalities.map((suggestion) => (
+                    <li key={suggestion.value} onClick={() => form.setValue('localities', suggestion.value)}>
+                      {suggestion.value}
+                    </li>
+                  ))}
+              </ul>
+            </div>
           </div>
-          <div>
-            <label>benefit usługa:</label>
-            <select {...register('benefit', { required: true })}>
-              <option value="" disabled selected>
-                Select benefit
-              </option>
-              {suggestions.length > 0 &&
-                suggestions.map((suggestion, idx) => (
-                  <option key={`${idx}-${suggestion}`} value={suggestion}>
-                    {suggestion}
-                  </option>
-                ))}
-            </select>
-            {errors.benefit && <span>This field is required</span>}
-          </div>
-          <div>
-            <label>Query city:</label>
-            <input type="text" {...register('queryCity')} />
-          </div>
-          <div>
-            <label>miasto:</label>
-            <select {...register('localities', { required: true })}>
-              <option value="" disabled selected>
-                Select city
-              </option>
-              {suggestionsLocalities.length > 0 &&
-                suggestionsLocalities.map((localities, idx) => (
-                  <option key={`${idx}-${localities}`} value={localities}>
-                    {localities}
-                  </option>
-                ))}
-            </select>
-            {errors.localities && <span>This field is required</span>}
-          </div>
-          <Button type="submit">Szukaj</Button>
-        </div>
-      </form>
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
 
       {isLoading && <p>Loading...</p>}
       {isError && <p>Error</p>}
