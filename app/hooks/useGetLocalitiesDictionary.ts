5b8050ea66b60a4a8ee23eff2c574f73c9247bb7
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-06-11 23:43:20 +0200

    Add useGetLocalitiesDictionary hook

---
+++ app/hooks/useGetLocalitiesDictionary.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+
+import { DictionaryAPI } from '@/api';
+import { LocalitiesDictionary } from '@/api/DictionaryAPI/Dictionary.model';
+import { QueryKey } from '@/api/queryKeys';
+import { transformResponseToForm } from '@/lib/utils';
+
+export const useGetLocalitiesDictionary = (localities: LocalitiesDictionary | null) => {
+  const { data, isLoading, isFetching, isError } = useQuery({
+    queryKey: [QueryKey.getLocalitiesDictionary],
+    queryFn: () => DictionaryAPI.getLocalitiesDictionary(localities!),
+    refetchOnWindowFocus: false,
+    enabled: !!localities && localities.length >= 3
+  });
+
+  return {
+    data: useMemo(() => (data ? transformResponseToForm(data) : []), [data]),
+    isLoading: isLoading || isFetching,
+    isError
+  };
+};
