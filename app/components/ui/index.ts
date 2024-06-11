2cdf14491a61e8e9a771f2a414cab5dc259c2733
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-06-11 22:32:01 +0200

    Remove unused components from radix

--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -2,8 +2,6 @@ export * from './button';
 export * from './label';
 export * from './radio-group';
 export * from './checkbox';
-export * from './command';
-export * from './popover';
 export * from './form';
 export * from './input';
 export * from './select';
